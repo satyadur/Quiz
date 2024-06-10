c5db25bb7737cc2b11dc0197e609daf2265e6098
THE NAME IS SATYA <satyaKudupudi8500@gmail.com>
2024-06-10 22:14:58 +0530

    deploy2

--- indes.js
+++ indes.js
@@ -112,27 +112,7 @@ const loadingAnimation = () => {
     }
   }, 500);
 };
-function defineProperty() {
-  var osccred = document.createElement("div");
-  osccred.innerHTML =
-    "A Project By <a href='https://www.youtube.com/@opensourcecoding' target=_blank>Open Source Coding</a>";
-  osccred.style.position = "absolute";
-  osccred.style.bottom = "0";
-  osccred.style.right = "0";
-  osccred.style.fontSize = "10px";
-  osccred.style.color = "#ccc";
-  osccred.style.fontFamily = "sans-serif";
-  osccred.style.padding = "5px";
-  osccred.style.background = "#fff";
-  osccred.style.borderTopLeftRadius = "5px";
-  osccred.style.borderBottomRightRadius = "5px";
-  osccred.style.boxShadow = "0 0 5px #ccc";
-  document.body.appendChild(osccred);
-}
-
-defineProperty();
-
-const submitBtn = document.querySelector(".submit"),
+const submitBtn = document.querySelector(".submit");
   nextBtn = document.querySelector(".next");
 submitBtn.addEventListener("click", () => {
   checkAnswer();
